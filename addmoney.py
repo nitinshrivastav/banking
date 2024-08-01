67c1a066f3da551b083eaff73ef5883060f99b2f
NaveenSinghGaira <naveensinghgaira13@gmail.com>
2024-08-01 21:06:26 +0530

    By Naveen

--- addmoney.py
+++ addmoney.py
@@ -1,3 +1,14 @@
 def add_money():
-    #naveen
-    pass
+    import pandas as pd
+    details=pd.read_csv(r"C:\Users\navee\Desktop\Banking project\banking\bankdata.csv")
+    box=list(details['account'])
+    account_no=int(input("enter your account no: "))
+    re_enter_account_no=int(input("confirm your account no: "))
+    if (account_no==re_enter_account_no and account_no in box):
+        index=box.index(account_no)
+        new_balance=(details['balance'][index])    
+    else:
+        print("account no not matched")
+    Enter_amount=int(input("enter amount to add: "))
+    Final_balance=new_balance+Enter_amount
+    print("your amount have been added successfully and updated balance is", Final_balance) 
