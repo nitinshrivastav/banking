48d73dfaefc4886822984588e6015a19510ef8cc
NaveenSinghGaira <naveensinghgaira13@gmail.com>
2024-08-04 18:27:47 +0530

    By Naveen

--- addmoney.py
+++ addmoney.py
@@ -1,14 +1,14 @@
 def add_money():
     import pandas as pd
-    details=pd.read_csv(r"C:\Users\navee\Desktop\Banking project\banking\bankdata.csv")
-    box=list(details['account'])
+    details=pd.read_csv(r"C:\Users\navee\Desktop\banking\bankdata.csv")
     account_no=int(input("enter your account no: "))
     re_enter_account_no=int(input("confirm your account no: "))
-    if (account_no==re_enter_account_no and account_no in box):
-        index=box.index(account_no)
-        new_balance=(details['balance'][index])    
+    if (account_no==re_enter_account_no and re_enter_account_no in list(details['account'])):
+        Amount=int(input("enter amount to be added: "))
+    for i in details.index:
+        if(details['account'][i]==re_enter_account_no):          #Question can we remove this line
+            print("Your Final balance is: ",details['balance'][i]+Amount)            
     else:
-        print("account no not matched")
-    Enter_amount=int(input("enter amount to add: "))
-    Final_balance=new_balance+Enter_amount
-    print("your amount have been added successfully and updated balance is", Final_balance) 
+        print('account number not matched')
+    details=details.to_csv(r"C:\Users\navee\Desktop\banking\bankdata.csv",index=False)
+def add_money()
